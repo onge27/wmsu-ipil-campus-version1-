9c077b1dd1ac4cb1e4b751e5033d344902473185
idk <rheasusonhidalgo@gmail.com>
2026-05-04 15:21:54 +0800

    Update MAIL_PASSWORD in configuration

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 #  CONFIGURATION
 # ─────────────────────────────────────────────
 MAIL_USERNAME  = os.getenv("MAIL_USERNAME", "rheasusonhidalgo@gmail.com")
-MAIL_PASSWORD  = os.getenv("MAIL_PASSWORD", "kdfi uhto wplg bhqy")
+MAIL_PASSWORD  = os.getenv("MAIL_PASSWORD", "aukd rlee dful wcqn")
 MAIL_FROM_NAME = os.getenv("MAIL_FROM_NAME", "ExamSys")
 SECRET_KEY     = os.getenv("SECRET_KEY", "changeme_super_secret_key_2025")
 DB_PATH        = os.getenv("DB_PATH", "examdb.sqlite")
